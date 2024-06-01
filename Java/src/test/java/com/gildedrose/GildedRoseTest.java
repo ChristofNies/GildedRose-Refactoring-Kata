e101ca1daaac73803fb0f31310e59264e1de45e9
ChristofNies <christof.nies@gmail.com>
2024-06-01 08:17:46 +0200

    Golden Master for testing legacy code

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,25 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+    void goldenMasterRefactoringTest() throws IOException {
+        Path file = Path.of("src/test/resources/GoldenMaster.txt");
+        String content = Files.readString(file);
 
+        ByteArrayOutputStream refactoredOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(refactoredOutput));
+        TexttestFixture.main(new String[]{});
+
+        assertEquals(refactoredOutput.toString(), content);
+    }
 }
